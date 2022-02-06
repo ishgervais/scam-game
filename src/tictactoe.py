92138d57ac3f05eceb4f2cdebb57cdd6c8009e36
ishgervais <ishimwegervais11@gmail.com>
2022-02-06 22:31:24 +0200

    feat: moved function in welcome function

--- src/tictactoe.py
+++ src/tictactoe.py
@@ -14,6 +14,7 @@ markers = [
 def welcome():
     """Function to welcome new player"""
     print("Welcome to TicTacToe")
+    fn()
     print("Here is a sample board,\nremember the positions of board")
     draw_board([" 0", "1", "2", " 3", "4", "5", " 6", "7", "8"])
 
